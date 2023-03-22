38b09942927104076f9af614365460436a0727c8
rhyckelm <122226150+rhyckelm@users.noreply.github.com>
2023-03-22 14:56:56 +0000

    add Ex02

--- Atividade 02/Ex02.py
+++ Atividade 02/Ex02.py
@@ -1,1 +1,21 @@
+agenda = {}
 
+while True:
+    cpf = input('Digite o CPF: ')
+
+    if not cpf:
+        break
+    
+    nome = input('Digite o nome: ')
+    idade = input('Digite a idade: ')
+    telefone = input('Digite o telefone: ')
+    
+    agenda[cpf] = {
+        'nome': nome,
+        'idade': idade,
+        'telefone': telefone
+    }
+
+print('Agenda:')
+for cpf, dados in agenda.items():
+    print(f"{dados['nome']}-{dados['idade']}-{dados['telefone']}")
