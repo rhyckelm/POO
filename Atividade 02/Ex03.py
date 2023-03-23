2b67beeb4eb0032b93efb80377c074fd67e859ad
rhyckelm <122226150+rhyckelm@users.noreply.github.com>
2023-03-22 22:03:07 -0300

    Update Ex03.py

--- Atividade 02/Ex03.py
+++ Atividade 02/Ex03.py
@@ -1,1 +1,18 @@
+principal = {}
+backup = {}
 
+while True:
+    chave = input("Digite a chave do dicionário: ")
+    valor = input("Digite o valor para armaxenar: ")
+    principal[chave] = valor
+    backup[chave] = valor
+
+    if len(principal) == 5:
+        print("Dados do dicionário principal:")
+        print(principal)
+        principal = {}
+
+    if len(backup) == 10:
+        print("Backup:")
+        print(backup)
+        backup = {}
