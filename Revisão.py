badb8cbbec626c069f256b1f7a7fc642298c2213
rhyckelm <122226150+rhyckelm@users.noreply.github.com>
2023-04-12 18:26:11 -0300

    Update and rename Revisão to Revisão.py

--- Revisão.py
+++ Revisão.py
@@ -24,38 +24,47 @@ def cadastrar():
             break
 
 
-'''def pesquisar():
+def pesquisar(cpf):
     indice_pessoas = {i["cpf"]: i for i in lista}
-    nome_procurado = input("Digite o cpf: ")
-    print(indice_pessoas[nome_procurado])'''
+    print(indice_pessoas[cpf])
 
-   
-'''def tel_novo():
-    cpf = input('Digite o cpf: ')
+
+def tel_novo(cpf):
     for i in lista:
-        if(cpf == i['cpf']):
+        if cpf == i["cpf"]:
             telefone = input("Telefone: ")
             funcionario["telefone"].append(telefone)
-            print('Telefone novo adicionado')
+            print("Telefone novo adicionado")
         else:
-            print('Cpf nao encontrado.')'''
-            
-def editar(cpf, **kwargs):
+            print("Cpf nao encontrado.")
+
+
+def editar(cpf):
     for i in lista:
-        if(cpf == i['cpf']):
-            cargo = input('Cargo: ')
-            i['cargo'] = cargo
-            salario = input()
-            i['salario'] = salario
+        if cpf == i["cpf"]:
+            cargo = input("Cargo: ")
+            i["cargo"] = cargo
+            salario = float(input("Salario: "))
+            i["salario"] = salario
             telefone = input("Telefone: ")
-            i['telefone'] = telefone
+            i["telefone"].clear()
+            i["telefone"].append(telefone)
+            print("Funcionario editado")
+            return
+    print("Cpf nao encontrado.")
 
-            print('Funcionario editado')
-        else:
-            print('Cpf nao encontrado.')
 
+def remover(cpf):
+    for i in range(len(lista)):
+        if cpf == lista[i]["cpf"]:
+            del lista[i]
+            print("Funcionario removido")
+            return
+    print("Cpf nao encontrado.")
 
 
 cadastrar()
+cpf = input("Digite o cpf: ")
+remover(cpf)
 
 print(lista)
